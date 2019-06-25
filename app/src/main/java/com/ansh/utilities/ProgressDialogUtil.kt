bec64b191005c5a501b24476cf488bba93dc1516
himanshupiplani <himanshu.piplani@mind-infotech.com>
2019-06-25 15:22:04 +0530

    loader ui

--- app/src/main/java/com/ansh/utilities/ProgressDialogUtil.kt
+++ app/src/main/java/com/ansh/utilities/ProgressDialogUtil.kt
@@ -5,6 +5,8 @@ import android.os.Handler
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import com.ansh.R
+import com.ansh.data.model.CurveDTO
+import com.ansh.view.custom.CurvesLoader
 import kotlinx.android.synthetic.main.layout_progress_dialog.view.*
 
 object ProgressDialogUtil {
@@ -20,6 +22,30 @@ object ProgressDialogUtil {
     fun show(activity: Activity, message: String? = null) {
         if (alertDialog == null) {
             createDialog(activity, message)
+        }
+        alertDialog?.let {
+            if (!it.isShowing && !activity.isFinishing) {
+                it.show()
+            }
+        }
+    }
+
+    fun show(activity: Activity, curveDTO: CurveDTO) {
+        if (alertDialog == null) {
+            val curveLoader = CurvesLoader(
+                activity,
+                curveDTO.noOfCurves,
+                curveDTO.outermostCurveRadius,
+                curveDTO.curveWidth,
+                curveDTO.distanceBetweenCurve,
+                curveDTO.curveSweepAngle,
+                curveDTO.color
+            )
+            alertDialog = AlertDialog
+                .Builder(activity, R.style.TransparentProgressDialog)
+                .setView(curveLoader)
+                .setCancelable(false)
+                .create()
         }
         alertDialog?.let {
             if (!it.isShowing && !activity.isFinishing) {
